279cfbba8a974504ca7f39ac629499045f84a375
JohnsonManuel <johnsonmanuel321@gmail.com>
2025-01-05 02:34:33 +0100

    Multi-Agent-Team

--- multi_agent.py
+++ multi_agent.py
@@ -1,45 +1,49 @@
-
 from phi.agent import Agent
+from phi.tools.hackernews import HackerNews
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.newspaper4k import Newspaper4k
 from phi.model.groq import Groq
-from phi.tools.youtube_tools import YouTubeTools
-from phi.tools.googlesearch import GoogleSearch
-
 from dotenv import load_dotenv
 
 # Loading the environment variables
 import os
 load_dotenv()
 
-google_search_agent = Agent(
-    tools=[GoogleSearch()],
-    description="You are a news agent that helps users find the latest news.",
+
+hn_researcher = Agent(
+    name="HackerNews Researcher",
+    role="Gets top stories from hackernews.",
+    tools=[HackerNews()],
+    model = Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+)
+
+web_searcher = Agent(
+    name="Web Searcher",
+    role="Searches the web for information on a topic",
+    tools=[DuckDuckGo()],
+    add_datetime_to_instructions=True,
+    model = Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+)
+
+article_reader = Agent(
+    name="Article Reader",
+    role="Reads articles from URLs.",
+    tools=[Newspaper4k()],
+    model = Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+)
+
+hn_team = Agent(
+    name="Hackernews Team",
+    team=[hn_researcher, web_searcher, article_reader],
     instructions=[
-        "Given a topic by the user, respond with 4 latest news items about that topic.",
-        "Search for 10 news items and select the top 4 unique items.",
+        "First, search hackernews for what the user is asking about.",
+        "Then, ask the article reader to read the links for the stories to get more information.",
+        "Important: you must provide the article reader with the links to read.",
+        "Then, ask the web searcher to search for each story to get more information.",
+        "Finally, provide a thoughtful and engaging summary.",
     ],
     model = Groq(id="llama3-groq-70b-8192-tool-use-preview"),
-    # show_tool_calls=True,
-    debug_mode=True,
+    show_tool_calls=True,
+    markdown=True,
 )
-
-youtube_agent = Agent(
-    name ="YouTube agent",
-    role="You are an YouTube agent that is used to fetch the captions of a YouTube video and retun key points.",
-    tools=[YouTubeTools()],
-    model = Groq(id="llama3-groq-70b-8192-tool-use-preview"),
-    # show_tool_calls=True,
-)
-
-# creative_blog_agent = Agent(
-#     name="Creative Blog Generator",
-#     team=[google_search_agent, youtube_agent],
-#     instructions=[
-#         "First, use the YouTube agent to retrieve captions from the provided YouTube video.",
-#         "Then, analyze the captions to extract key ideas, notable quotes",
-#     ],
-#     model = Groq(id="llama3-groq-70b-8192-tool-use-preview"),
-#     # show_tool_calls=True,
-#     markdown=True,
-#     debug_mode=True,
-# )
-youtube_agent.print_response("Create a blog post from the video https://www.youtube.com/watch?v=4LeBTYpfgS4 in 300 words", markdown=True)
+hn_team.print_response("Write an article about the top 2 stories on hackernews")
